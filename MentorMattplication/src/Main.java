f90255ab0c7f8242a1e2aff9584b813a789b4ae0
Chrostor <pierce_loren@yahoo.com>
2016-03-23 21:25:26 -0700

    Initial Commit - Window opens, animation works

---
+++ MentorMattplication/src/Main.java
@@ -0,0 +1,50 @@
+
+
+import javafx.animation.Animation;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+
+public class Main extends Application {
+
+    private Image IMAGE = new Image(getClass().getClassLoader().getResource("runningcat.png").toExternalForm());
+
+    private static final int COLUMNS  =   2;
+    private static final int COUNT    =  8;
+    private static final int OFFSET_X =  0;
+    private static final int OFFSET_Y =  10;
+    private static final int WIDTH    = 512;
+    private static final int HEIGHT   = 256;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("The Horse in Motion");
+
+        final ImageView imageView = new ImageView(IMAGE);
+        imageView.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
+
+        final Animation animation = new SpriteAnimation(
+                imageView,
+                Duration.millis(400),
+                COUNT, COLUMNS,
+                OFFSET_X, OFFSET_Y,
+                WIDTH, HEIGHT
+        );
+        animation.setCycleCount(Animation.INDEFINITE);
+        animation.play();
+
+        primaryStage.setScene(new Scene(new Group(imageView)));
+        primaryStage.show();
+    }
+}
